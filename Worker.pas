716e75d42158c20aa98d39cd7ccbbb58f534b249
Michał Jasiak <mjasiak@pl.sii.eu>
2017-02-15 09:34:28 +0100

    Edycja

--- Worker.pas
+++ Worker.pas
@@ -4,7 +4,7 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Grids, Vcl.DBGrids, ConnectionModule, User, DeleteDialog;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Grids, Vcl.DBGrids, ConnectionModule, User, DeleteDialog, EditDialog;
 
 type
   TWorkerForm = class(TForm)
@@ -41,7 +41,7 @@ end;
 
 procedure TWorkerForm.btn_editClick(Sender: TObject);
 begin
-     //
+     EditForm.ShowForm(ChooseUser);
 end;
 
 procedure TWorkerForm.btn_mycardClick(Sender: TObject);
